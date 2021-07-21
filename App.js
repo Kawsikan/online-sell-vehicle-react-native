d796edbb2c02dc3e61117efd411f3c0eccd0864f
kawsikan <kawsinote@gmail.com>
2021-07-22 01:27:37 +0530

    added tab navigation

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ListVehcile from './components/Home/ListVehcile';
+import ListBikes from './components/Home/ListBikes';
+import ListParts from "./components/Home/ListParts";
 
+const Tab = createBottomTabNavigator();
 
 const App = () => {
 
     return (
-        <View>
-            <Text>Hello</Text>
-
-        </View>
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Vehicle" component={ListVehcile} />
+                <Tab.Screen name="Bikes" component={ListBikes} />
+                <Tab.Screen name="Parts" component={ListParts} />
+            </Tab.Navigator>
+        </NavigationContainer>
     )
 }
 
