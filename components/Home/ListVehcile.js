4221f9370b7d26e84ff01620ac6348046f671f29
kawsikan <kawsinote@gmail.com>
2021-07-25 22:36:57 +0530

    chnage to listvehicle

--- components/Home/ListVehcile.js
+++ components/Home/ListVehcile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, View, Image, SafeAreaView, StatusBar, FlatList } from 'react-native';
 
 const ListVehcile = () => {
 
@@ -7,11 +7,32 @@ const ListVehcile = () => {
 
     return (
         <View>
-            <Text>
-                Vehicles
-            </Text>
+
+            <Image
+                source={{
+                    uri: 'https://live.staticflickr.com/1920/29913654857_f130040b7a_b.jpg',
+                }}
+                style={{
+                    width: 70, height: 70
+                }}
+            />
+
         </View>
     )
-}
 
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
+});
 export default ListVehcile
